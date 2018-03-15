23c24d5ce3c4e7281d06eb70a14041a71835fa63
Jared Lunde <jared.lunde@gmail.com>
2018-03-15 14:03:22 -0600

    Ensures immutability for ViewportProvider context value

--- packages/viewport/src/ViewportProvider.js
+++ packages/viewport/src/ViewportProvider.js
@@ -5,8 +5,40 @@ import ViewportContext from './ViewportContext'
 
 
 export default function ViewportProvider (props) {
+  let prevContext = {}
+
   return Viewport({
     children: function (context) {
+      if (
+        prevContext.scrollY !== context.scrollY
+        || prevContext.aspect !== context.aspect
+        || prevContext.scrollX !== context.scrollX
+      ) {
+        // ensures immutability for the provider value
+        context = {
+          inFullView: context.inFullView,
+          inFullViewX: context.inFullViewX,
+          inFullViewY: context.inFullViewY,
+          inView: context.inView,
+          inViewX: context.inViewX,
+          inViewY: context.inViewY,
+
+          scrollTo: context.scrollTo,
+          scrollX: context.scrollX,
+          scrollY: context.scrollY,
+          direction: context.direction,
+          distance: context.distance,
+
+          orientation: context.orientation,
+          screenOrientation: context.screenOrientation,
+          height: context.height,
+          width: context.width,
+          aspect: context.aspect,
+        }
+        
+        prevContext = context
+      }
+
       return (
         <ViewportContext.Provider value={context}>
           {props.children}
